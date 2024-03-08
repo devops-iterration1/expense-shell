fde3623f3b675ddebb88f660e007d3b78e6e684a
izypsy <reddy.nie11@gmail.com>
2024-03-09 00:44:57 +0530

    adding reverse proxy to fe

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,8 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 
+cp expense.conf /etc/nginx/default.d/expense.conf
+
 cd /usr/share/nginx/html/
 unzip /tmp/frontend.zip
 
