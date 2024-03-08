b536a7359d326f0fcc4476db8169e02500b052ac
izypsy <reddy.nie11@gmail.com>
2024-03-09 00:38:36 +0530

    nginx

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,7 @@ dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
 
-rm -rf /usr/share/ngnix/html/*
+rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 
